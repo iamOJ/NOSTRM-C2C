b879d3b0ac325e11efaf8d9ef040877d92c2e23e
iamOJ <ojaswi.kumar@outlook.com>
2018-03-11 15:35:30 +0530

    Read JSON into ListView

--- JSONReceive/app/src/main/java/com/jones/jsonreceive/MainActivity.java
+++ JSONReceive/app/src/main/java/com/jones/jsonreceive/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private String url = "http://192.168.43.24:4100/mi";
 
     ArrayList<HashMap<String, String>> contactList;
+    ArrayList<HashMap<String, String>> blockList;
     SwipeRefreshLayout sf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         contactList = new ArrayList<>();
+        blockList = new ArrayList<>();
 
         lv = (ListView) findViewById(R.id.list);
         sf = findViewById(R.id.swiperefresh);
@@ -58,11 +60,142 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void check(View v){
+        if(!sf.isRefreshing())
+            sf.setRefreshing(true);
         EditText et = (EditText) findViewById(R.id.edit);
         url = et.getText().toString();
         Log.i("url",url);
         Log.i("what","button pressed");
-        new GetContacts().execute();
+        //new GetContacts().execute();
+        new GetBlocks().execute();
+    }
+
+    private class GetBlocks extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    //JSONArray contacts = jsonObj.getJSONArray("transactions");
+                    //JSONArray contacts = jsonObj.getJSONArray("cc");
+                    JSONArray blocks = jsonObj.getJSONArray("blocks");
+                    System.out.print(blocks.toString());
+                    Log.e(TAG, "Response from url: " + blocks.toString());
+                    // looping through All Contacts
+                    blockList.clear();
+                    for (int i = 0; i < blocks.length(); i++) {
+                        JSONObject c = blocks.getJSONObject(i);
+
+                        String index = c.getString("index");
+                        String prev_hash = c.getString("prev_hash");
+                        String time = c.getString("timestamp");
+                        JSONObject data = c.getJSONObject("data");
+                        //JSONArray data = c.getJSONArray("data");
+                        String proof="";
+                        try {
+                            proof = data.getString("proof-of-work");
+                        }catch(Exception e){
+                            e.printStackTrace();
+                        }
+                        JSONArray trans = new JSONArray();
+                        try {
+                            trans = data.getJSONArray("transactions");
+
+                        }catch(Exception e){
+                            e.printStackTrace();
+                        }
+                        Log.i("proof",proof);
+                        Log.i("transactions",trans.toString());
+                        /*
+                        // Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");
+                        */
+                        // tmp hash map for single contact
+                        HashMap<String, String> block = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        block.put("index", index);
+                        block.put("prev_hash", prev_hash);
+                        block.put("timestamp", time);
+
+
+                        // adding contact to contact list
+
+                        blockList.add(block);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+            if(sf.isRefreshing())
+                sf.setRefreshing(false);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, blockList,
+                    R.layout.list_item, new String[]{"index", "prev_hash",
+                    "timestamp"}, new int[]{R.id.index,
+                    R.id.prev, R.id.time});
+
+            lv.setAdapter(adapter);
+        }
     }
 
     private class GetContacts extends AsyncTask<Void, Void, Void> {
@@ -175,8 +308,8 @@ public class MainActivity extends AppCompatActivity {
             ListAdapter adapter = new SimpleAdapter(
                     MainActivity.this, contactList,
                     R.layout.list_item, new String[]{"name", "age",
-                    "aa"}, new int[]{R.id.name,
-                    R.id.age, R.id.aa});
+                    "aa"}, new int[]{R.id.index,
+                    R.id.prev, R.id.time});
 
             lv.setAdapter(adapter);
         }
