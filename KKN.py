59f02257fbccf5123b64a0d740ad934d0dc3643e
Axhk97m <mandhar.anirudh@gmail.com>
2018-03-12 11:08:41 +0530

    Add files via upload
    KNN and Collaborative Filtering For Book Recommendation

---
+++ KKN.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+from scipy.sparse import csr_matrix
+import sklearn
+from sklearn.decomposition import TruncatedSVD
+
+book = pd.read_csv('BX-Books.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+book.columns = ['ISBN', 'bookTitle', 'bookAuthor', 'yearOfPublication', 'publisher', 'imageUrlS', 'imageUrlM', 'imageUrlL']
+user = pd.read_csv('BX-Users.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+user.columns = ['userID', 'Location', 'Age']
+rating = pd.read_csv('BX-Book-Ratings.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+rating.columns = ['userID', 'ISBN', 'bookRating']
+
+rating.head()
+user.head()
+book.head()
+combine_book_rating = pd.merge(rating, book, on='ISBN')
+columns = ['yearOfPublication', 'publisher', 'bookAuthor', 'imageUrlS', 'imageUrlM', 'imageUrlL']
+combine_book_rating = combine_book_rating.drop(columns, axis=1)
+combine_book_rating.head()
+combine_book_rating = combine_book_rating.dropna(axis = 0, subset = ['bookTitle'])
+book_ratingCount = (combine_book_rating.
+     groupby(by = ['bookTitle'])['bookRating'].
+     count().
+     reset_index().
+     rename(columns = {'bookRating': 'totalRatingCount'})
+     [['bookTitle', 'totalRatingCount']]
+    )
+book_ratingCount.head()
+rating_with_totalRatingCount = combine_book_rating.merge(book_ratingCount, left_on = 'bookTitle', right_on = 'bookTitle', how = 'left')
+rating_with_totalRatingCount.head()
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+print(book_ratingCount['totalRatingCount'].describe())
+print(book_ratingCount['totalRatingCount'].quantile(np.arange(.9, 1, .01)))
+popularity_threshold = 10
+rating_popular_book = rating_with_totalRatingCount.query('totalRatingCount >= @popularity_threshold')
+rating_popular_book.head()
+combined = rating_popular_book.merge(user, left_on = 'userID', right_on = 'userID', how = 'left')
+
+india_user_rating = combined[combined['Location'].str.contains("india")]
+india_user_rating=india_user_rating.drop('Age', axis=1)
+india_user_rating.head()
+if not india_user_rating[india_user_rating.duplicated(['userID', 'bookTitle'])].empty:
+    initial_rows = india_user_rating.shape[0]
+
+    print('Initial dataframe shape {0}'.format(india_user_rating.shape))
+    india_user_rating = india_user_rating.drop_duplicates(['userID', 'bookTitle'])
+    current_rows = india_user_rating.shape[0]
+    print('New dataframe shape {0}'.format(india_user_rating.shape))
+    print('Removed {0} rows'.format(initial_rows - current_rows))
+    india_user_rating_pivot = india_user_rating.pivot(index = 'bookTitle', columns = 'userID', values = 'bookRating').fillna(0)
+india_user_rating_matrix = csr_matrix(india_user_rating_pivot.values)
+from sklearn.neighbors import NearestNeighbors
+
+model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
+model_knn.fit(india_user_rating_matrix)
+query_index = np.random.choice(india_user_rating_pivot.shape[0])
+distances, indices = model_knn.kneighbors(india_user_rating_pivot.iloc[query_index, :].reshape(1, -1), n_neighbors = 6)
+
+for i in range(0, len(distances.flatten())):
+    if i == 0:
+        print('Recommendations for {0}:\n'.format(india_user_rating_pivot.index[query_index]))
+    else:
+        print('{0}: {1}, with distance of {2}:'.format(i, india_user_rating_pivot.index[indices.flatten()[i]], distances.flatten()[i]))
+india_user_rating_pivot2 = india_user_rating.pivot(index = 'userID', columns = 'bookTitle', values = 'bookRating').fillna(0)
+india_user_rating_pivot2.head()
+india_user_rating_pivot2.shape
+X = india_user_rating_pivot2.values.T
+X.shape
+
+SVD = TruncatedSVD(n_components=12, random_state=17)
+matrix = SVD.fit_transform(X)
+matrix.shape
+
+
+import warnings
+warnings.filterwarnings("ignore", category =RuntimeWarning)
+corr = np.corrcoef(matrix)
+corr.shape
+india_user_book_title = india_user_rating_pivot2.columns
+india_user_book_list = list(india_user_book_title)
+coffey_hands = india_user_book_list.index("Someone's Watching")
+print(coffey_hands)
+corr_coffey_hands = corr[coffey_hands]
+list(india_user_book_title[(corr_coffey_hands<1.0) & (corr_coffey_hands>0.8)])
